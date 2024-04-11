f1127e8f0c0c79b7c3060af96092ef1548598947
cwcinc <ovsecrets10@gmail.com>
2024-04-10 22:34:59 -0700

    s

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.environ("TOKEN"))
+client.run(os.getenv("TOKEN"))
