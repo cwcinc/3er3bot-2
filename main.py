352c2b989575906e34ab8bd395b66f1de3a0832a
cwcinc <ovsecrets10@gmail.com>
2024-04-10 22:55:20 -0700

    change

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import discord, os
+from discord import app_commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
@@ -16,5 +19,14 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+@tree.command(
+    name="FiRsT",
+    description="My first application Command",
+    guild=discord.Object(id=856733214659051520)
+)
+async def first_command(interaction):
+    await interaction.response.send_message("Hello yayyy!")
+
 
 client.run(os.getenv("TOKEN"))
