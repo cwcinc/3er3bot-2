baebe8d3baed63edba9ec0ef2a7bae7e3af633b6
cwcinc <ovsecrets10@gmail.com>
2024-04-10 22:59:44 -0700

    w

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ client = discord.Client(intents=intents)
 
 tree = app_commands.CommandTree(client)
 
+@tree.command(
+    name="FiRsT",
+    description="My first application Command"
+)
+async def first_command(interaction):
+    await interaction.response.send_message("Hello yayyy!")
+
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=856733214659051520))
     print(f'We have logged in as {client.user}')
 
 @client.event
@@ -19,14 +27,6 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-
-@tree.command(
-    name="FiRsT",
-    description="My first application Command",
-    guild=discord.Object(id=856733214659051520)
-)
-async def first_command(interaction):
-    await interaction.response.send_message("Hello yayyy!")
 
 
 client.run(os.getenv("TOKEN"))
