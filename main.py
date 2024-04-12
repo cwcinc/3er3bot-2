866295b27798e880418c70638a9e5a0c1b9a6dde
cwcinc <ovsecrets10@gmail.com>
2024-04-11 21:56:32 -0700

    add stuff

--- main.py
+++ main.py
@@ -1,54 +1,35 @@
 import discord, os
-from discord import app_commands
+from discord.ui import View, Button
+
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # Needed for slash commands
 
-client = discord.Client(intents=intents)
+bot = discord.Client(intents=intents)
 
-tree = app_commands.CommandTree(client)
+tree = discord.app_commands.CommandTree(bot)
 
-
-class Menu(discord.ui.View):
-    def __init__(self):
-        super.__init__()
-        self.value = None
-
-    @discord.ui.button(label="Test", style=discord.ButtonStyle.grey)
-    async def menu1(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.send_message("YAy you clicked test!")
+guildID = 856733214659051520
 
 
-@tree.command(
-    name="first",
-    description="My first application Command",
-    guild=discord.Object(id=856733214659051520)
-)
-async def first(ctx, title: str, description: str):
-    embed=discord.Embed(title=title, description=description, color=discord.Color.blue())
-    embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Latin_letter_small_capital_ezh.svg/1200px-Latin_letter_small_capital_ezh.svg.png")
-    await ctx.response.send_message(embed=embed, ephemeral=True)
+class MyView(View):
+    @discord.ui.button(label="Click Me!", style=discord.ButtonStyle.green)
+    async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # Update button text or perform actions on click
+        button.label = "Clicked!"
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send("Button Clicked!", ephemeral=True)
 
 
-async def second(ctx):
-    view = Menu()
-    await ctx.response.send_message(view=view, ephemeral=True)
+@tree.command(name="bal", description="Find your own 3er3coin balance!", guild=discord.Object(id=guildID))
+async def test_command(interaction: discord.Interaction):
+    view = MyView()
+    await interaction.response.send_message("This message has buttons!", view=view)
 
-
-@client.event
+@bot.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=856733214659051520))
+    await tree.sync(guild=discord.Object(id=guildID))
     print("Enabled!!!")
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-"""
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-"""
-
-client.run(os.getenv("TOKEN"))
+bot.run(os.getenv("TOKEN"))
